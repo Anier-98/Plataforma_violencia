fccb59952e068ed210204561005903bf84a5c6a3
Anier-98 <anierf3@gmail.com>
2021-03-02 15:13:13 -0600

    eliminacion de espacio

--- backend/ConexioBD.php
+++ backend/ConexioBD.php
@@ -1,5 +1,4 @@
 <?php
-
 $db = new PDO('mysql:host=127.0.0.1;dbname=violencia','root','');
 $db->setAttribute (PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 //$filas = $db->query('SELECT * FROM area;');
