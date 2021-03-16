76cdee22cf26764f8892d0b770ca0660f33e93b7
Bielka099 <098joseangel@gmail.com>
2021-03-16 09:16:22 -0600

    .

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,4 +1,4 @@
-var violence = document.getElementById("violencia").hidden=true;
+var violence = document.getElementById("violencia").hidden = true;
 var bt1 = document.getElementById("btn1");
 var txt = document.getElementById("intr");
 var cls = document.getElementById("cerrar");
@@ -7,7 +7,7 @@ bt1.addEventListener('click',mostrar_violencia_test);
 cls.addEventListener('click',ocultar_violencia_test);
 
 function mostrar_violencia_test(){
-    violence = document.getElementById("violencia").hidden= false;
+    violence = document.getElementById("violencia").hidden = false;
     window.location.href='#violencia';
     txt.focus();
 }
@@ -16,3 +16,15 @@ function ocultar_violencia_test(){
     violence = document.getElementById("violencia").hidden= true;
 }
 
+var juridico = document.getElementById("mj").hidden = true;
+var b1=document.getElementById("botn1");
+
+var txt = document.getElementById("introj");
+var b2 = document.getElementById("botn2");
+
+b1.addEventListener('click',mostrar_regla_estudiantil);
+function mostrar_regla_estudiantil(){
+    juridico = document.getElementById("mj").hidden= false;
+    window.location.href='#mj';
+    juridico.focus();
+}
