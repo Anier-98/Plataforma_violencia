412e643e41e135adddab146a82f710b0c9b45618
Bielka099 <098joseangel@gmail.com>
2021-03-16 14:12:16 -0600

    ..

--- js/juridico.js
+++ js/juridico.js
@@ -2,10 +2,9 @@ var juridico = document.getElementById("mj").hidden = true;
 var b1 = document.getElementById("botn1");
 b1.addEventListener('click',mostrar_regla_estudiantil);
 
-
 function mostrar_regla_estudiantil(){
     juridico = document.getElementById("mj").hidden= false;
     window.location.href='#mj';
-    juridico.focus;
+    juridico.focus();
 }
 
